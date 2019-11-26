e5e8ceeb6e06e0ac0137965d8189c7ed8b214544
Nadia Auger <nadia.auger0@gmail.com>
2019-11-26 18:21:03 +0100

    top top

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_153428) do
+ActiveRecord::Schema.define(version: 2019_11_26_163243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 2019_11_25_153428) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.date "start"
-    t.date "end"
+    t.datetime "start"
+    t.datetime "end"
     t.integer "max_particpants"
     t.text "description"
     t.bigint "hospital_id"
