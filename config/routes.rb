13d7beff87cdbafac2afa52c67156755668a0b88
Nadia Auger <nadia.auger0@gmail.com>
2019-11-27 15:41:21 +0100

    notification new registration done

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
- 
- 
+
+
   resources :users, only: [:show]
   resources :events, only: [:show, :new, :create,:edit, :update] do
     resources :reservations, only: [:create, :destroy]
   end
- 
+
   resources :hospitals, only: [:show]
- 
+
   get '/dashboard', to: 'users#dashboard', as: 'dashboard'
+  get '/notification', to: 'users#notification', as: 'notification'
 
 end
