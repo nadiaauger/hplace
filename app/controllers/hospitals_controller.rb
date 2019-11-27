0e80dc84842f277f007977d07097fef44b35b803
Leo Chaput <leo.chaput@hotmail.fr>
2019-11-27 12:48:16 +0100

    search bar is now working

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,7 +1,16 @@
 class HospitalsController < ApplicationController
-skip_before_action :authenticate_user!, only: [:show]
+skip_before_action :authenticate_user!, only: [:show, :handler]
 
   def show
      @hospital = Hospital.find(params[:id])
+  end
+
+  def handler
+    @hospital = Hospital.find_by(name: params[:query])
+    if @hospital != nil
+      redirect_to hospital_path(@hospital)
+    else
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
- 
- 
+
+
   resources :users, only: [:show]
   resources :events, only: [:show, :new, :create,:edit, :update] do
     resources :reservations, only: [:create, :destroy]
   end
- 
+
   resources :hospitals, only: [:show]
- 
+
+  get '/handler', to: 'hospitals#handler', as: 'handler'
   get '/dashboard', to: 'users#dashboard', as: 'dashboard'
+
 
 end
