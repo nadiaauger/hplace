b0137f54ae368a810eeeae95e9be97dec0c54dad
Juliette Hoppenot <juliette.hoppenot@gmail.com>
2019-11-27 13:02:57 +0100

    changed label size in form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap";
- 
+import "../plugins/flatpickr";
 import "./event_form";
- 
+
 // var typed = new Typed('#typed', {
 //   stringsElement: '#typed-strings', typeSpeed: 100
 // });
