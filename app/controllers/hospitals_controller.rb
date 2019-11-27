a8823c5a9992a5d6c95c07b63990f05ca330cc70
Louis Lescure <louis.lescure@mail.mcgill.ca>
2019-11-27 11:50:42 +0100

    added links to navbar and class active

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -2,6 +2,6 @@ class HospitalsController < ApplicationController
 skip_before_action :authenticate_user!, only: [:show]
 
   def show
-     @hospital = Hospital.find(params[:id])
+    @hospital = Hospital.find(params[:id])
   end
 end
