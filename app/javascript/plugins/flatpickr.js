b89fb5973a9c10fa4724e918bc2d3efbdc5839a1
Nadia Auger <nadia.auger0@gmail.com>
2019-11-28 11:28:33 +0100

    ok

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,3 +3,9 @@ import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
 flatpickr(".datepicker");
 //console.log(document.querySelector(".datepicker"));
+flatpickr(".timepicker", {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    time_24hr: true
+});
