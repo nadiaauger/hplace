6c5ff9978640233207accae9922e260296ade0c9
Leo Chaput <leo.chaput@hotmail.fr>
2019-11-29 15:18:30 +0100

    finish almost chat

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ import { eventFormExport } from "../plugins/event_form";
 //   stringsElement: '#typed-strings', typeSpeed: 100
 // });
 import { typedStringsExport } from "../plugins/typed";
-import "../components/photo_preview";
 
 import { chatRefresh } from "../plugins/init_action_cable";
 import { receivedCall } from "../plugins/init_action_cable";
