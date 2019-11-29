acbfa1e61b1379d9b981f80410bba2f886733554
Nadia Auger <nadia.auger0@gmail.com>
2019-11-29 15:34:03 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_29_112625) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +84,17 @@ ActiveRecord::Schema.define(version: 2019_11_29_112625) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "recipient_id"
+    t.string "action"
+    t.string "notifiable_type"
+    t.integer "notifiable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
   create_table "reservations", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "event_id"
@@ -120,6 +129,7 @@ ActiveRecord::Schema.define(version: 2019_11_29_112625) do
   add_foreign_key "events", "users"
   add_foreign_key "messages", "chat_rooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "notifications", "users"
   add_foreign_key "reservations", "events"
   add_foreign_key "reservations", "users"
   add_foreign_key "users", "hospitals"
