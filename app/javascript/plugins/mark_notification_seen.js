ac0d0ebe9416b6b7921b1fadc5db65d5d5b6a6c8
Nadia Auger <nadia.auger0@gmail.com>
2019-12-04 19:07:36 +0100

    ok

--- app/javascript/plugins/mark_notification_seen.js
+++ app/javascript/plugins/mark_notification_seen.js
@@ -1,6 +1,8 @@
 
 const markNotificationSeen = () => {
   const toogleNotif = document.querySelector(".toggle-notifications");
+  const bell = document.querySelector(".toggle-notifications");
+
   var i = 0;
   if (toogleNotif){
     toogleNotif.addEventListener('click', () => {
@@ -8,18 +10,31 @@ const markNotificationSeen = () => {
       badge.style.display = "";
       const formNotif = document.querySelector("#form-notification-seen");
       Rails.fire(formNotif, 'submit');
+      // body.classList.add('body-grise')
       // This is a disgusting hack, please forgive me. this marks all unseen elements
       // as seen upon the second click to see the notifications.
-      console.log(i);
       if (i > 0) {
+        console.log(i);
         document.querySelectorAll(".unseen").forEach(function(element) {
           element.classList.remove("unseen");
           element.classList.add("seen");
         });
+
       }
       i++
+      griseBody();
     });
   }
 }
+const griseBody = () => {
+  const body = document.querySelector(".body-to-grey");
+  console.log(document.querySelector(".helping").classList.value.includes("show"))
+  if (document.querySelector(".helping").classList.value.includes("show")){
+    body.classList.remove('body-grise');
+  }
+  else {
+    body.classList.add('body-grise');
+  }
+};
 
 export { markNotificationSeen };
