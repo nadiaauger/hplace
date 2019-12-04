c434aab9b97ec5c17deb9ab3669095e624e188b3
Juliette Hoppenot <juliette.hoppenot@gmail.com>
2019-12-04 11:49:06 +0100

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -133,6 +133,8 @@ file = URI.open('https://images.pexels.com/photos/1073097/pexels-photo-1073097.j
 benoit.hospital = st_louis
 benoit.photo.attach(io: file, filename: 'benoit.jpg', content_type: 'image/jpg')
 benoit.save
+
+
  file = URI.open('https://images.pexels.com/photos/1906997/pexels-photo-1906997.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
    tom = User.new ({
     username:      'Tom',
@@ -154,7 +156,7 @@ tom.save
 chloe.photo.attach(io: file, filename: 'chloe.jpg', content_type: 'image/jpg')
 chloe.save
  file = URI.open('https://images.unsplash.com/photo-1533101585792-27f81a845550?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')
- 
+
    roger = User.new ({
     username:      'Roger',
     room:      '12',
@@ -165,14 +167,12 @@ chloe.save
 roger.photo.attach(io: file, filename: 'roger.jpg', content_type: 'image/jpg')
 roger.save
 file = URI.open('https://images.unsplash.com/photo-1444069069008-83a57aac43ac?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80')
- 
+
    michel = User.new ({
     username:      'Michel',
     room:      '15',
     email: 'michel@gmail.com',
     password: '123456'
- 
-
   })
 
    michel.hospital = st_louis
@@ -189,7 +189,7 @@ file = URI.open('https://images.unsplash.com/photo-1566616213894-2d4e1baee5d8?ix
     room:      '15',
     email: 'anna@gmail.com',
     password: '123456'
- 
+
 
   })
 
@@ -207,7 +207,7 @@ file = URI.open('https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ix
     room:      '15',
     email: 'sophie@gmail.com',
     password: '123456'
- 
+
   })
 
    sophie.hospital = st_louis
@@ -222,7 +222,7 @@ file = URI.open('https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ix
     room:      '15',
     email: 'eddie@gmail.com',
     password: '123456'
- 
+
 
   })
 
@@ -234,7 +234,7 @@ eddie.save
  puts 'Finished!'
 puts 'Creating events...'
 file = URI.open('https://images.unsplash.com/photo-1501003878151-d3cb87799705?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')
- 
+
    belote = Event.new ({
     name:      'Belote',
     location:      'Cafeteria',
@@ -249,7 +249,7 @@ belote.hospital = st_louis
 belote.user = leo
 belote.photo.attach(io: file, filename: 'belote.jpg', content_type: 'image/jpg')
 belote.save
- 
+
 
 file = URI.open('https://images.pexels.com/photos/1329645/pexels-photo-1329645.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
 
@@ -262,12 +262,12 @@ file = URI.open('https://images.pexels.com/photos/1329645/pexels-photo-1329645.j
     max_participants: 4,
     description: 'Je vous propose une petite partie de monopoly pour se détendre, dans la bonne humeur. Je ramène le jeu !'
   })
- 
+
 monopoly.hospital = st_louis
-monopoly.user = cyril
+monopoly.user = roger
 monopoly.photo.attach(io: file, filename: 'monopoly.jpg', content_type: 'image/jpg')
 monopoly.save
- 
+
 
 file = URI.open('https://www.regles-de-jeux.com/wp-content/uploads/2017/12/regle-bridge-1024x768.jpg')
 
@@ -278,16 +278,16 @@ file = URI.open('https://www.regles-de-jeux.com/wp-content/uploads/2017/12/regle
     start:          DateTime.new(2019,12,7,15),
     end:          DateTime.new(2019,12,7,18),
     max_participants: 4,
-    description: 'Je vous propose une petite partie de bridge pour se détendre, dans la bonne humeur. Je ramène le jeu !'
+    description: 'Je vous invite à une petite partie de bridge pour se détendre, dans la bonne humeur. Je ramène le jeu !'
   })
- 
 
- 
+
+
 bridge.hospital = st_louis
 bridge.user = cyril
 bridge.photo.attach(io: file, filename: 'bridge.jpg', content_type: 'image/jpg')
 bridge.save
- 
+
 
  file = URI.open('https://images.unsplash.com/photo-1568270363382-992bd101f48e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')
    tricot = Event.new ({
@@ -325,7 +325,7 @@ file = URI.open('https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ix
     description: "On m'a offert une Playstation ce weekend, qui veut venir jouer à Fifa avec moi ??"
   })
 fifa.hospital = st_louis
-fifa.user = leo
+fifa.user = nadia
 fifa.photo.attach(io: file, filename: 'fifa.jpg', content_type: 'image/jpg')
 fifa.save
 file = URI.open('https://images.unsplash.com/photo-1535016120720-40c646be5580?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')
@@ -348,18 +348,241 @@ file = URI.open('https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ix
     start:         DateTime.new(2019,12,7,15),
     end:         DateTime.new(2019,12,7,17),
     max_participants: 8,
-    description: 'L\'année dernière, j\'ai fait une formation intensive en dévelopment web. J\'adorerais partager ce que j\'ai appris avec ceux qui seraient intéressé. Ma spécialité etait le Javascript. Au menu: comment coder un super boutons avec de effets de ouf!'
+    description: 'L\'année dernière, j\'ai fait une formation intensive en dévelopment web. J\'adorerais partager ce que j\'ai appris avec ceux qui seraient intéressés. Ma spécialité etait le Javascript. Au menu: comment coder un super boutons avec de effets de ouf!',
   })
 code.hospital = st_louis
 code.user = leo
 code.photo.attach(io: file, filename: 'code.jpg', content_type: 'image/jpg')
 code.save
+
+
+file = URI.open('https://images.pexels.com/photos/167446/pexels-photo-167446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+
+
+    chant = Event.new ({
+    name:      'Chant à capella',
+    location:      'Cafeteria',
+    start:          DateTime.new(2019,12,7,15),
+    end:          DateTime.new(2019,12,7,18),
+    max_participants: 4,
+    description: 'Il paraît que tout le monde a une voix qui vaut le détour! Retrouvons-nous pour pousser la chansonnette !',
+  })
+
+chant.hospital = st_louis
+chant.user = anna
+chant.photo.attach(io: file, filename: 'chant.jpg', content_type: 'image/jpg')
+chant.save
+
+file = URI.open('https://images.pexels.com/photos/2420585/pexels-photo-2420585.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+
+
+    discussion = Event.new ({
+    name:      'discussion sur la pluie et le beau temps',
+    location:      'Cafeteria',
+    start:          DateTime.new(2019,12,17,15),
+    end:          DateTime.new(2019,12,17,17),
+    max_participants: 10,
+    description: 'Nous sommes pas mal de nouveaux patients, et si on se retrouvait pour faire connaissance?',
+  })
+
+discussion.hospital = st_louis
+discussion.user = tom
+discussion.photo.attach(io: file, filename: 'discussion.jpg', content_type: 'image/jpg')
+discussion.save
+
+file = URI.open('https://images.pexels.com/photos/713149/pexels-photo-713149.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+
+
+    impro = Event.new ({
+    name:      'Initiation au théêtre d\'impro',
+    location:      'Cafeteria',
+    start:          DateTime.new(2019,12,23,15),
+    end:          DateTime.new(2019,12,23,18),
+    max_participants: 8,
+    description: 'Le théâtre d\'impro est excellent pour la santé ! On essaie ensemble ? Même si vous êtes timide venez quand même !'
+  })
+
+impro.hospital = st_louis
+impro.user = jeanne
+impro.photo.attach(io: file, filename: 'impro.jpg', content_type: 'image/jpg')
+impro.save
+
+file = URI.open('https://images.pexels.com/photos/973403/pexels-photo-973403.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+
+
+    coiffure = Event.new ({
+    name:      'Apprendre à jouer avec sa coiffure',
+    location:      'Ma chambre, 207',
+    start:          DateTime.new(2019,12,22,10),
+    end:          DateTime.new(2019,12,22,13),
+    max_participants: 6,
+    description: 'Changer de coiffure fait du bien au moral ! Je peux vous montrer comment se faire des tresses indiennes super jolies en quelques minutes',
+  })
+
+coiffure.hospital = st_louis
+coiffure.user = clemence
+coiffure.photo.attach(io: file, filename: 'coiffure.jpg', content_type: 'image/jpg')
+coiffure.save
+
+file = URI.open('https://images.pexels.com/photos/6453/nature-flowers-plant-spring.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+
+
+    blabla = Event.new ({
+    name:      'Simplement faire connaissance',
+    location:      'Cafeteria',
+    start:          DateTime.new(2019,12,21,10),
+    end:          DateTime.new(2019,12,21,13),
+    max_participants: 8,
+    description: 'Je suis nouveau à l\'hôpital et je serai content de pouvoir passer du temps avec vous'
+  })
+
+blabla.hospital = st_louis
+blabla.user = chloe
+blabla.photo.attach(io: file, filename: 'blabla.jpg', content_type: 'image/jpg')
+blabla.save
+
+file = URI.open('https://images.pexels.com/photos/6453/nature-flowers-plant-spring.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+
+
+    bataille = Event.new ({
+    name:      'Bataille corse',
+    location:      'Salle 203',
+    start:          DateTime.new(2019,12,14,10),
+    end:          DateTime.new(2019,12,14,12),
+    max_participants: 4,
+    description: 'Des intéressés par une petite bataille corse ? '
+  })
+
+bataille.hospital = st_louis
+bataille.user = stephane
+bataille.photo.attach(io: file, filename: 'bataille.jpg', content_type: 'image/jpg')
+bataille.save
+
+file = URI.open('https://images.pexels.com/photos/1753411/pexels-photo-1753411.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+
+
+    tisane = Event.new ({
+    name:      'Tisane de Noël',
+    location:      'Salon du RDC',
+    start:          DateTime.new(2019,12,21,17),
+    end:          DateTime.new(2019,12,21,18),
+    max_participants: 4,
+    description: 'La tisane de l\'amitié, juste avant Noël. Je dispose de plusieurs tisanes délicieuses à vous faire goûter.'
+  })
+
+tisane.hospital = st_louis
+tisane.user = sophie
+tisane.photo.attach(io: file, filename: 'tisane.jpg', content_type: 'image/jpg')
+tisane.save
+
+file = URI.open('https://images.pexels.com/photos/381739/pexels-photo-381739.jpeg?cs=srgb&dl=beautiful-bloom-blooming-blossom-381739.jpg&fm=jpg')
+
+
+    jardin = Event.new ({
+    name:      'Promenade dans le jardin',
+    location:      'Jardin',
+    start:          DateTime.new(2019,12,21,14),
+    end:          DateTime.new(2019,12,21,15),
+    max_participants: 8,
+    description: 'Une petite promenade digestive fait toujours plaisir, qui se joint à moi?'
+  })
+
+jardin.hospital = st_louis
+jardin.user = jean
+jardin.photo.attach(io: file, filename: 'jardin.jpg', content_type: 'image/jpg')
+jardin.save
+
+file = URI.open('https://images.pexels.com/photos/259698/pexels-photo-259698.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+
+
+    chanson = Event.new ({
+    name:      'Chantons ensemble',
+    location:      'Refectoire',
+    start:          DateTime.new(2019,12,20,15),
+    end:          DateTime.new(2019,12,20,17),
+    max_participants: 15,
+    description: 'L\'équipe médicale organise pour nous une session de chants de Noël, ça va être super beau, il faut être nombreux !'
+  })
+
+chanson.hospital = st_louis
+chanson.user = benoit
+chanson.photo.attach(io: file, filename: 'chanson.jpg', content_type: 'image/jpg')
+chanson.save
+
+
+file = URI.open('https://images.pexels.com/photos/1708601/pexels-photo-1708601.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+
+
+    depart = Event.new ({
+    name:      'Mon départ de la clinique',
+    location:      'Cafeteria',
+    start:          DateTime.new(2019,12,18,15),
+    end:          DateTime.new(2019,12,18,17),
+    max_participants: 15,
+    description: 'Chers tous, ça y est je quitte la clinique après 4 mois de soins intensifs. Je vous invite à un petit goûter pour fêter ça.'
+  })
+
+depart.hospital = st_louis
+depart.user = eddie
+depart.photo.attach(io: file, filename: 'depart.jpg', content_type: 'image/jpg')
+depart.save
+
+
+file = URI.open('https://images.pexels.com/photos/1708601/pexels-photo-1708601.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+
+
+    priere = Event.new ({
+    name:      'Prière de Noël',
+    location:      'Salon',
+    start:          DateTime.new(2019,12,25,15),
+    end:          DateTime.new(2019,12,25,17),
+    max_participants: 10,
+    description: 'Prions ensemble pour Noël et pour garder espoir'
+  })
+
+priere.hospital = st_louis
+priere.user = anna
+priere.photo.attach(io: file, filename: 'priere.jpg', content_type: 'image/jpg')
+priere.save
+
+
+file = URI.open('https://images.pexels.com/photos/1257105/pexels-photo-1257105.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+
+
+    lecture = Event.new ({
+    name:      'Lecture pour enfants',
+    location:      'chambre 46',
+    start:          DateTime.new(2019,12,22,15),
+    end:          DateTime.new(2019,12,22,17),
+    max_participants: 5,
+    description: 'Mon fils sera ravi de partager ses livres avec les autres enfants et de lire tous ensemble'
+  })
+
+lecture.hospital = st_louis
+lecture.user = sarah
+lecture.photo.attach(io: file, filename: 'lecture.jpg', content_type: 'image/jpg')
+lecture.save
+
 puts 'Finished!'
 puts 'Creating reservations...'
 reserve = Reservation.new
 reserve.user = nadia
 reserve.event = belote
 reserve.save
+
+reserve = Reservation.new
+reserve.user = nadia
+reserve.event = impro
+reserve.save
+reserve = Reservation.new
+reserve.user = roger
+reserve.event = impro
+reserve.save
+reserve = Reservation.new
+reserve.user = clemence
+reserve.event = impro
+reserve.save
+
 reserve = Reservation.new
 reserve.user = tom
 reserve.event = belote
