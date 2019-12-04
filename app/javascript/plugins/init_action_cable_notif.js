47556956d7bed1105d5bf17ce6b721ccf63ff046
Nadia Auger <nadia.auger0@gmail.com>
2019-12-04 12:34:09 +0100

    standby pour le complet ou non d'event

--- app/javascript/plugins/init_action_cable_notif.js
+++ app/javascript/plugins/init_action_cable_notif.js
@@ -20,10 +20,12 @@ const displayNotifications = (data) => {
 
 const notifRefresh = () => {
   const notif = document.querySelector('#show-notif');
-  App[`notification_for_user_${notif.dataset.user}`] = App.cable.subscriptions.create(
-    { channel: 'NotificationForUsersChannel', user_id: notif.dataset.user },
-    { received: displayNotifications }
-  );
+  if (notif){
+    App[`notification_for_user_${notif.dataset.user}`] = App.cable.subscriptions.create(
+      { channel: 'NotificationForUsersChannel', user_id: notif.dataset.user },
+      { received: displayNotifications }
+    );
+  }
 }
 
 export { notifRefresh };
--- app/javascript/plugins/mark_notification_seen.js
+++ app/javascript/plugins/mark_notification_seen.js
@@ -2,22 +2,24 @@
 const markNotificationSeen = () => {
   const toogleNotif = document.querySelector(".toggle-notifications");
   var i = 0;
-  toogleNotif.addEventListener('click', () => {
-    const badge = document.querySelector(".button_badge");
-    badge.style.display = "";
-    const formNotif = document.querySelector("#form-notification-seen");
-    Rails.fire(formNotif, 'submit');
-    // This is a disgusting hack, please forgive me. this marks all unseen elements
-    // as seen upon the second click to see the notifications.
-    console.log(i);
-    if (i > 0) {
-      document.querySelectorAll(".unseen").forEach(function(element) {
-        element.classList.remove("unseen");
-        element.classList.add("seen");
-      });
-    }
-    i++
-  });
+  if (toogleNotif){
+    toogleNotif.addEventListener('click', () => {
+      const badge = document.querySelector(".button_badge");
+      badge.style.display = "";
+      const formNotif = document.querySelector("#form-notification-seen");
+      Rails.fire(formNotif, 'submit');
+      // This is a disgusting hack, please forgive me. this marks all unseen elements
+      // as seen upon the second click to see the notifications.
+      console.log(i);
+      if (i > 0) {
+        document.querySelectorAll(".unseen").forEach(function(element) {
+          element.classList.remove("unseen");
+          element.classList.add("seen");
+        });
+      }
+      i++
+    });
+  }
 }
 
 export { markNotificationSeen };
